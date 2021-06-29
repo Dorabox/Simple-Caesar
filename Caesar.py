79ad16be89002744d5924dbeebf7b856427bb0e6
Dorabox <85057511+Dorabox@users.noreply.github.com>
2021-06-29 11:06:33 +0800

    Cleaned up functions
    I mad the code look a lot more presentable.

--- Caesar.py
+++ Caesar.py
@@ -2,27 +2,25 @@ lex = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','
 word = input('Type the statement you want to code:')
 wordarr = list(word)
 shift = int(input('Type the shift number you want:'))
-indx = []
-new = []
-new_word =""
-def decrypting(lex, indx, shift):
-    decrypted_list = []
-    for i in indx:
-        shifted = i%26
+indx =[]
+result = ""
+def to_num():
+    for letter in word:
+        for let in lex:
+            if (letter == let):
+                indx.append(lex.index(let) + shift)
+def to_word():
+    new_word = []
+    for num in indx:
+        shifted = num%26
         new_val = lex[shifted]
-        decrypted_list.append(new_val)
-    return decrypted_list
-def encrypting():
-  for letter in wordarr:
-      for i in lex: 
-        if (letter == i):
-          indx.append(lex.index(i)+shift)
-  
-if set(wordarr).issubset(lex) is  True:
-  encrypt = encrypting()
-  decrypt = decrypting(lex=lex, indx=indx, shift=shift)
-  new_word = new_word.join(decrypt)
-  print(new_word)
-else:
-    print("Invalid input")
+        new_word.append(new_val)
+    return new_word
 
+if set(wordarr).issubset(lex) is True:
+    to_num = to_num()
+    to_word = to_word()
+    result = result.join(to_word)
+    print(result)
+else:
+    print("Invalid Input")
